6ebcd06ec58e09481357846cd7ba181dda59007e
os2 <os2@gmail.com>
2025-02-27 08:49:19 +0700

    tao list user co button an hien

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,22 +1,42 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUser: true,
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser,
+        });
+    }
     render() {
         //props => properties
         //destructuring array/object
         const { listUser } = this.props;
+        // console.log(listUser);
         return (
             <div>
-                {
-                    listUser.map((user) => {
-                        return (
-                            <div key={user.id}>
-                                <div>My name is {user.name}</div>
-                                <div>I'm {user.age} years old</div>
-                                <hr />
-                            </div>
-                        );
-                    })
+                {/* button */}
+                <div>
+                    <button onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser ? 'Hide' : 'Show'} List User
+                    </button>
+                </div>
+                {/* render list user */}
+                {this.state.isShowListUser &&
+                    <div>
+                        {
+                            listUser.map((user) => {
+                                return (
+                                    <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
+                                        <div>My name is {user.name}</div>
+                                        <div>I'm {user.age} years old</div>
+                                        <hr />
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
                 }
             </div>
         );
