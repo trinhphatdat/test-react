6d0dab47cc68d8eabf28566e05862db11f01add9
trinhphatdat <dattrinh1922@gmail.com>
2025-03-05 20:33:40 +0700

    life cycle

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -2,15 +2,36 @@ import React from "react";
 import './DisplayInfo.scss';
 import logo from './../logo.svg';
 class DisplayInfo extends React.Component {
-    state = {
-        isShowListUser: true,
+    constructor(props) {
+        console.log('>>> call constructor: 1')
+        super(props);
+
+        this.state = {
+            isShowListUser: true,
+        }
     }
+
+    componentDidMount() {
+        console.log('>>> call me component did mount');
+        setTimeout(() => {
+            document.title = 'test';
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component did update', this.props, prevProps);
+        if (this.props.listUser !== prevProps.listUser)
+            if (this.props.listUser.length == 5)
+                alert('You got 5 user');
+    }
+
     handleShowOrHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser,
         });
     }
     render() {
+        console.log('>>> call me render: ')
         //props => properties
         //destructuring array/object
         const { listUser } = this.props;
