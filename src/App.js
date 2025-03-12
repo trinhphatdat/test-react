940fd3fe018095136966030c8d2e1c896c45f909
trinhphatdat <dattrinh1922@gmail.com>
2025-03-12 19:50:55 +0700

    import react-bootstrap and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './component/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           {/* Edit <code>src/App.js</code> and save to reload. */}
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
